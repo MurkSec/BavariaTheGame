b474d4d3d955468b2da5968b68952c63fa439436
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2018-08-08 12:40:34 -0500

    added screenwipe function

--- helper_functions.py
+++ helper_functions.py
@@ -12,3 +12,14 @@ def WrappedTextOutput(p_text, p_width=70):
 def clear_screen():
   print('\033[H\033[J')
   time.sleep(0.1)
+
+"""
+Screenwipe
+
+description: This will wipe the screen for multiple OS versions.
+params: operator  - sets the operator to be rendered to screen      | defailt "\n"
+        lines     - literally prints the operator x number of times | default 250
+return: void
+"""
+def Screenwipe(operator="\n", lines=250)#
+  print('\n'*250)
