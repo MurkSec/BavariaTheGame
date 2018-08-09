f946eb6e29c51b57dbf6ef54e995aa77dd67ad64
gwa2100 <gwa2100@gmail.com>
2018-08-09 00:04:15 -0500

    Fixed undocumented we addweapon bug. was not referencing player object.

--- class_world.py
+++ class_world.py
@@ -441,7 +441,7 @@ class gameWorld:
       if tmp.lower() == "y":
         if self.player.gold >= rdn:
           print(' It will serve you well!')
-          self.addWeapon(wpn.wname, wpn.watk, wpn.w_hit, wpn.wType)
+          self.player.addWeapon(wpn.wname, wpn.watk, wpn.w_hit, wpn.wType)
           print(f'   ****{wpn.wname} equiped****')
           self.player.gold -= rdn
         else:
@@ -479,7 +479,7 @@ class gameWorld:
       if tmp.lower() == "y":
         if self.player.gold >= rdn:
           print(' It will serve you well!')
-          self.GiveItem(itm.iname)
+          self.player.GiveItem(itm.iname)
           print(f'   ****{itm.iname} added to your inventory****')
           self.player.gold -= rdn
         else:
