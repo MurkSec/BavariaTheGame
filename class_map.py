9063af5de3d3f7efd31201f6b910977cb4765a8e
redwiz666 <redwiz666@gmail.com>
2018-08-09 05:44:48 -0500

    inital map creation

---
+++ class_map.py
@@ -0,0 +1,13 @@
+import random
+
+
+
+def createMap():
+    maxX = 15
+    maxY = 15
+    Entrence = (0,0)
+    Boss = (random.randint(maxX-random.randint(0-5),maxY-random.randint(0-5)),random.randint(maxX-random.randint(0-5),maxY-random.randint(0-5)))
+    
+    maxTunnels = 12
+    maxLength = 12
+
