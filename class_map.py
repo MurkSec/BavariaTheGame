cd249ef7b1fa1a686a1c5312efcc59a5175e4c00
redwiz666 <41351318+redwiz666@users.noreply.github.com>
2018-08-09 07:44:33 -0500

    Update class_map.py

--- class_map.py
+++ class_map.py
@@ -1,13 +1,35 @@
 import random
 
+'''
+This will return map=[[0,0,0,1,0,0,0,1],
+                      [0,0,0,1,0,0,0,1]]
+
+reference place by map[1][2]
+
+'''
 
 
 def createMap():
+    #define variables
+    map=[]
+    rows=[]
+    cols=[]
     maxX = 15
     maxY = 15
     Entrence = (0,0)
     Boss = (random.randint(maxX-random.randint(0-5),maxY-random.randint(0-5)),random.randint(maxX-random.randint(0-5),maxY-random.randint(0-5)))
     
-    maxTunnels = 12
-    maxLength = 12
+    #Cycle throu each Row
+    for row in range(0,maxX):
+        #Cycle throu each Column
+        for col in range(0,maxY):
+            #Add a value to the column
+            cols.append("1")
+        #Add the Column to the row
+        row.append(cols)
+    #
+    map.append(row)
+    [print(row) for row in self.map]
+    return map
 
+    
