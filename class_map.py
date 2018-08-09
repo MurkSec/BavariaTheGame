35cc8359949fa674dc8c5b84329113fca39303dd
redwiz666 <41351318+redwiz666@users.noreply.github.com>
2018-08-09 08:10:44 -0500

    Update class_map.py

--- class_map.py
+++ class_map.py
@@ -17,7 +17,7 @@ def createMap():
     maxX = 15
     maxY = 15
     Entrence = (0,0)
-    Boss = (random.randint(maxX-random.randint(0-5),maxY-random.randint(0-5)),random.randint(maxX-random.randint(0-5),maxY-random.randint(0-5)))
+    #Boss = (random.randint(maxX-random.randint(0-5),maxY-random.randint(0-5)),random.randint(maxX-random.randint(0-5),maxY-random.randint(0-5)))
     
     #Cycle throu each Row
     for row in range(0,maxX):
